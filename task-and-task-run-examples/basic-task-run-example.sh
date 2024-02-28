5547aecf097feeef56467ef8d3240e1ddd3793f4
tluwaniMS <tluwani2020@gmail.com>
2024-02-28 07:35:49 +0200

    adding-basic-task-scripts

--- task-and-task-run-examples/basic-task-run-example.sh
+++ task-and-task-run-examples/basic-task-run-example.sh
@@ -2,4 +2,6 @@
 
 # This bash script is responsible for running and setting up all the components of the task-greeter task.
 
-echo "task-greeter task"
+kubectl apply -f ./task-and-task-run-examples/basic-task-example.yaml
+
+kubectl apply -f ./task-and-task-run-examples/basic-task-run-example.yaml
--- task-and-task-run-examples/volume-task-run-example.sh
+++ task-and-task-run-examples/volume-task-run-example.sh
@@ -2,4 +2,6 @@
 
 # This bash script is responsible for running and setting up all the components of the volume-example task.
 
-echo "volume-example task"
+kubectl apply -f ./task-and-task-run-examples/volume-task-example.yaml
+
+kubectl apply -f ./task-and-task-run-examples/volume-task-run-example.yaml
