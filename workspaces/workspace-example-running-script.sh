757b8f1245056f74a97e3d993d12bee25d99135c
tluwaniMS <tluwani2020@gmail.com>
2024-02-28 07:40:33 +0200

    adding-workspace-scripts

--- workspaces/workspace-example-running-script.sh
+++ workspaces/workspace-example-running-script.sh
@@ -2,4 +2,10 @@
 
 # This bash script is responsible for running and setting up all the components of the dynastic-pipeline.
 
-echo "dynastic-pipeline"
+kubectl apply -f ./workspaces/mwenemutapa-workspace-tasks.yaml
+
+kubectl apply -f ./workspaces/nguni-workspace-tasks.yaml
+
+kubectl apply -f ./workspaces/workspace-pipeline.yaml
+
+kubectl apply -f ./workspaces/workspace-pipeline-run.yaml
