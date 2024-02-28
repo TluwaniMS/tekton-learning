1f1440853e9e6e1bda9ec99e3f125213c80ef7cf
tluwaniMS <tluwani2020@gmail.com>
2024-02-28 07:38:37 +0200

    adding-task-results-script

--- tekton-results/tekton-results-example-running-script.sh
+++ tekton-results/tekton-results-example-running-script.sh
@@ -3,4 +3,8 @@
 
 # This bash script is responsible for running and setting up all the components of the arithmetic-results-pipeline.
 
-echo "arithmetic-results-pipeline"
+kubectl apply -f ./tekton-results/tekton-results-task.yaml
+
+kubectl apply -f ./tekton-results/tekton-results-pipeline.yaml
+
+kubectl apply -f ./tekton-results/tekton-results-pipeline-run.yaml
